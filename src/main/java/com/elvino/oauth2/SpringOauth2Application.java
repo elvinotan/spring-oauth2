d1171350c35384ebeb5d7c884e7e3e956fbc1233
elvino <elvino_tan@yahoo.com>
2019-06-05 01:56:06 +0700

    initial

--- src/main/java/com/elvino/oauth2/SpringOauth2Application.java
+++ src/main/java/com/elvino/oauth2/SpringOauth2Application.java
@@ -3,8 +3,12 @@ package com.elvino.oauth2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import com.elvino.oauth2.config.CustomAuthenticationProvider;
 
 @SpringBootApplication
 public class SpringOauth2Application {
